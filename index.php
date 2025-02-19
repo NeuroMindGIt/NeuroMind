955956916f4969494aa5777162fae32e864c7039
NeuroMindGit <neuromindgit@gmail.com>
2025-02-19 20:47:05 -0300

    primeira mensagem

---
+++ index.php
@@ -0,0 +1,4 @@
+
+<?php
+    include './pages/cadastro.php'; //incluindo pagina de cadastro, não modificar esse index, ATT. Vinícius W.
+?>
---
+++ pages/cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="assets/icons/logo.png">
+    <title>Cadastro CareConnect</title>
+</head>
+<body class="bodycadastro">
+    <img src="assets/icons/logo.png" class="logo-cadastro">
+    <h3>CADASTRE-SE</h3>
+    <br>
+    <label>Nome</label>
+    <input type="text" placeholder="Digite seu nome:" id="nome-user" class="input-cadastro">
+    <br>
+    <label>Email</label>
+    <input type="email" placeholder="Digite seu email:" id="email-user" class="input-cadastro">
+    <br>
+    <label>Senha</label>
+    <input type="password" placeholder="Digite sua senha:" id="senha-user" class="input-cadastro">
+    <p>Já possui uma conta? entre agora!</p>
+    <button class="submit-cad" id="submit-cad">Cadastar</button>
+</body>
+</html>
