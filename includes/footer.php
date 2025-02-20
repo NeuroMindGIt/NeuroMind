d69f40f222c50ffbf801e009bf59ab0a36acb9b4
viniciuswmbv <viniciuswcontato@gmail.com>
2025-02-19 22:49:12 -0300

    Add itens na tela de cadastro

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>feito pela equipe &copycordex / todos os direitos reservados</p>
+</footer>
--- pages/cadastro.php
+++ pages/cadastro.php
@@ -2,24 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="css/main.css">
     <link rel="icon" href="assets/icons/logo.png">
     <title>Cadastro CareConnect</title>
 </head>
 <body class="bodycadastro">
+    <main class="maincadastro">
     <img src="assets/icons/logo.png" class="logo-cadastro">
-    <h3>CADASTRE-SE</h3>
+    <h4>Seja bem-vindo ao NeuroMind!</h4>
     <br>
+    <h5>CADASTRE-SE</h5>
+    <br>
+    <form action="cadastro.php" method="post">
     <label>Nome</label>
+    <br>
     <input type="text" placeholder="Digite seu nome:" id="nome-user" class="input-cadastro">
-    <br>
+    <br><br>
     <label>Email</label>
-    <input type="email" placeholder="Digite seu email:" id="email-user" class="input-cadastro">
     <br>
+    <input type="email" placeholder="Digite seu email:" id="email-user" class="input-cadastro">
+    <br><br>
     <label>Senha</label>
+    <br>
     <input type="password" placeholder="Digite sua senha:" id="senha-user" class="input-cadastro">
-    <p>Já possui uma conta? entre agora!</p>
+    <p>Já possui uma conta? <a href="pages/login.php">Clique aqui!</a></p>
+    <br>
     <button class="submit-cad" id="submit-cad">Cadastar</button>
+    
+    <?php 
+        include './includes/footer.php';
+    ?>
+</main>
+</form>
 </body>
 </html>
