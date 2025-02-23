fb18c3c27b8e563c3760d38f697fc0b3291edb80
viniciuswmbv <viniciuswcontato@gmail.com>
2025-02-23 02:28:58 -0300

    add styles do menu horizontal

---
+++ includes/menu-nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Mapa</a></li>
+        <li><a href="#">Calendario</a></li>
+        <li><a href="#">Chat-bot</a></li>
+        <li><a href="#">Exercicios</a></li>
+    </ul>
+</nav>
